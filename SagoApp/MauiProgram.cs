54466c27adc2961ef961e3e0f67db744e34b352e
Ermin Husic <Ermin.Husic@live.se>
2025-04-04 11:52:49 +0200

    added adMob

--- SagoApp/MauiProgram.cs
+++ SagoApp/MauiProgram.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.Extensions.Logging;
 using CommunityToolkit.Maui;
+using Plugin.AdMob;
+using Plugin.AdMob.Configuration;
 
 namespace SagoApp
 {
@@ -8,13 +10,18 @@ namespace SagoApp
         public static MauiApp CreateMauiApp()
         {
             var builder = MauiApp.CreateBuilder();
-            builder.UseMauiApp<App>().ConfigureFonts(fonts =>
+            builder
+                
+              .UseMauiApp<App>()
+              .UseAdMob()
+              .ConfigureFonts(fonts =>
             {
                 fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                 fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
             }).UseMauiCommunityToolkit();
 #if DEBUG
             builder.Logging.AddDebug();
+            AdConfig.UseTestAdUnitIds = true; // Använd testannons-ID:n under utveckling
 #endif
             return builder.Build();
         }
