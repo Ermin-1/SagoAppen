af6dd67ee62b3b292b5e67f55d09caf128401178
Ermin Husic <Ermin.Husic@live.se>
2025-04-22 08:56:48 +0200

    Working app before intigration of Maui.Media

--- SagoApp/Platforms/Android/MainActivity.cs
+++ SagoApp/Platforms/Android/MainActivity.cs
@@ -1,11 +1,51 @@
-﻿using Android.App;
+﻿//using Android.App;
+//using Android.Content.PM;
+//using Android.OS;
+//using Android.Views;
+
+//namespace SagoApp
+//{
+//    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+//    public class MainActivity : MauiAppCompatActivity
+//    {
+//    }
+//}
+
+
+
+using Android.App;
 using Android.Content.PM;
 using Android.OS;
+using Android.Views;
 
 namespace SagoApp
 {
-    [Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+    [Activity(Theme = "@style/Maui.SplashTheme",
+              MainLauncher = true,
+              LaunchMode = LaunchMode.SingleTop,
+              ConfigurationChanges = ConfigChanges.ScreenSize |
+                                     ConfigChanges.Orientation |
+                                     ConfigChanges.UiMode |
+                                     ConfigChanges.ScreenLayout |
+                                     ConfigChanges.SmallestScreenSize |
+                                     ConfigChanges.Density)]
     public class MainActivity : MauiAppCompatActivity
     {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Gör statusbaren transparent & tillåt layout bakom den
+            if (Build.VERSION.SdkInt >= BuildVersionCodes.Lollipop)
+            {
+                Window.ClearFlags(WindowManagerFlags.TranslucentStatus);
+                Window.AddFlags(WindowManagerFlags.DrawsSystemBarBackgrounds);
+                Window.SetStatusBarColor(Android.Graphics.Color.Transparent);
+
+                Window.DecorView.SystemUiVisibility = (StatusBarVisibility)(
+                    SystemUiFlags.LayoutStable |
+                    SystemUiFlags.LayoutFullscreen);
+            }
+        }
     }
 }
